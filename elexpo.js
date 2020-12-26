4a65b620439f160561b00a02cccfeb0c1acd080f
CommentatoreVideo <giangorn12345678@gmail.com>
2020-12-26 19:08:57 +0100

    Modificato link css elexpo

--- elexpo.js
+++ elexpo.js
@@ -2,6 +2,6 @@
   const head=document.getElementsByTagName("head")[0];
   const link=document.createElement("link");
   link.rel="stylesheet";
-  link.href="https://raw.githubusercontent.com/CommentatoreVideo/segnalibri/main/elexpo.css";
+  link.href="https://https://segnalibri.vercel.app/elexpo.css";
   head.appendChild(link);
 })();
