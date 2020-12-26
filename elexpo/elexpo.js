c9a06f9ff80797b1c4b809af49c35ebf3334a76f
CommentatoreVideo <giangorn12345678@gmail.com>
2020-12-26 20:58:59 +0100

    Aggiunta estensione Chrome

--- elexpo/elexpo.js
+++ elexpo/elexpo.js
@@ -1,7 +1,6 @@
-(function() {
+
   const head=document.getElementsByTagName("head")[0];
   const link=document.createElement("link");
   link.rel="stylesheet";
   link.href="https://segnalibri.vercel.app/elexpo.css";
   head.appendChild(link);
-})();
