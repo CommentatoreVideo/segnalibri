003f8dd39420619a202323d5034f4a17576a6cd0
CommentatoreVideo <giangorn12345678@gmail.com>
2020-12-26 18:05:05 +0100

    Aggiunto inizio elexpo

---
+++ elexpo.js
@@ -0,0 +1,7 @@
+(function() {
+  const head=document.getElementsByTagName("head")[0];
+  const link=document.createElement("link");
+  link.rel="stylesheet";
+  link.href="C:\\wamp64\\www\\css\\elexpo.css";
+  head.appendChild(link);
+})();
