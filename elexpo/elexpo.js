625c7c478cdf1325afe204dbfa89d7a7290996f6
CommentatoreVideo <giangorn12345678@gmail.com>
2020-12-26 21:16:25 +0100

    Iniziato lavoro editor testo elexpo

--- elexpo/elexpo.js
+++ elexpo/elexpo.js
@@ -1,6 +1,13 @@
+const head = document.getElementsByTagName("head")[0];
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://segnalibri.vercel.app/elexpo.css";
+head.appendChild(link);
 
-  const head=document.getElementsByTagName("head")[0];
+const iframe=document.querySelector("iframe");
+if(iframe) {
   const link=document.createElement("link");
   link.rel="stylesheet";
-  link.href="https://segnalibri.vercel.app/elexpo.css";
-  head.appendChild(link);
+  link.href="https://segnalibri.vercel.app/elexpoRisposte.css";
+  iframe.head.appendChild(link);
+}
