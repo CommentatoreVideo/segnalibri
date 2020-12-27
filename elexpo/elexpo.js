e2ec3b7a190a23ceab2fb35448700f3e23d582a4
CommentatoreVideo <giangorn12345678@gmail.com>
2020-12-27 21:19:51 +0100

    Text editor e altri fix

--- elexpo/elexpo.js
+++ elexpo/elexpo.js
@@ -4,10 +4,22 @@ link.rel = "stylesheet";
 link.href = "https://segnalibri.vercel.app/elexpo.css";
 head.appendChild(link);
 
-const iframe=document.querySelector("iframe");
-if(iframe) {
-  const link=document.createElement("link");
-  link.rel="stylesheet";
-  link.href="https://segnalibri.vercel.app/elexpo2.css";
-  iframe.head.appendChild(link);
+let iframe;
+let tentativi = 0;
+try {
+  console.log(document);
+  setTimeout(() => {
+    iframe = Array.from(document.getElementById("cke_1_contents").children)[1];
+    console.log("iframe");
+    console.log(iframe);
+    if(iframe) {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = "https://segnalibri.vercel.app/elexpo2.css";
+      iframe.contentWindow.document.head.appendChild(link);
+    }
+  }, 1000);
+} catch(e) {
+  tentativi++;
+  console.log(e);
 }
