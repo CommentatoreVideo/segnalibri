3acbad68d0acda2a66e17aaf4bbcc047892fba25
CommentatoreVideo <giangorn12345678@gmail.com>
2021-01-05 18:11:02 +0100

    Aggiunto codice umanet

---
+++ umanet/umanet.js
@@ -0,0 +1,5 @@
+const head = document.getElementsByTagName("head")[0];
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://segnalibri.vercel.app/umanet.css";
+head.appendChild(link);
